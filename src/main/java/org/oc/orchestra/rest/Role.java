7209d3f422b83882cb06053638ea6481a7dc277d
bigorc <fuhongwei141@gmail.com>
2015-08-29 12:41:29 +0800

    Add Role

---
+++ src/main/java/org/oc/orchestra/rest/Role.java
@@ -0,0 +1,13 @@
+package org.oc.orchestra.rest;
+
+import org.restlet.representation.Representation;
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+
+public class Role extends ServerResource {
+
+	@Get
+	protected Representation getRole() {
+		return null;
+	}
+}
